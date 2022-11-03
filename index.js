04b5e5bb6f1de4dcdcfae5fb05e030d5ef7ba9ed
Edwinn33 <Edwin3333>
2022-11-03 12:06:14 +0800

    Activity code

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const userRoutes = require("./routes/user.js")
+const courseRoutes = require("./routes/course.js")
+
+const app = express();
+
+
+mongoose.connect("mongodb+srv://admin:admin123@cluster0.uxmvvrn.mongodb.net/Course-Booking-API?retryWrites=true&w=majority", {
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+});
+
+mongoose.connection.once("open", () => console.log("Now connected to MongoDB Atlas"))
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
+//Initializing the routes
+app.use("/users", userRoutes);
+app.use("/courses", courseRoutes);
+
+
+app.listen(process.env.PORT || 4000, () => {
+	console.log(`API is now online on port ${process.env.PORT || 4000}`);
+})
+
+
